d8a034856486f01664c47fedcc3d452fa2684d98
JiaJingwei <jiajingwei@yunzhangfang.com>
2024-07-31 17:59:55 +0800

    Jwt的生成和解析

--- src/main/java/com/sasa/Main.java
+++ src/main/java/com/sasa/Main.java
@@ -5,6 +5,8 @@ import io.jsonwebtoken.Claims;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        long l = -9223372036854775808L;
+        System.out.println(l);
 
     }
 

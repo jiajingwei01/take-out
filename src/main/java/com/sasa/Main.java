52caf284c5a30f0dc81e2464471a9de1c7ec85db
JiaJingwei <jiajingwei@yunzhangfang.com>
2024-07-31 18:20:58 +0800

    master分支直接修改

--- src/main/java/com/sasa/Main.java
+++ src/main/java/com/sasa/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("master 的修改");
+        System.out.println("开发人员开发的项目");
+    }
+    public static void m1(){
+        System.out.println("开发人员开的东西！！！");
     }
 }

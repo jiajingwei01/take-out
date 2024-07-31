249cb135ba33ad8e6ff542650e8566460488d8af
JiaJingwei <jiajingwei@yunzhangfang.com>
2024-07-31 18:35:26 +0800

    小红需求的代码

--- src/main/java/com/sasa/Main.java
+++ src/main/java/com/sasa/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("master 的修改");
+
+    }
+    public static void xiaoHongCreateMethod(){
+        System.out.println("小红需求的代码");
     }
 }

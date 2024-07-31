6732f9ac5686f63dd2f034aeac3abfdc1ce4f421
JiaJingwei <jiajingwei@yunzhangfang.com>
2024-07-31 18:26:20 +0800

    小红的代码

--- src/main/java/com/sasa/Main.java
+++ src/main/java/com/sasa/Main.java
@@ -5,10 +5,15 @@ import io.jsonwebtoken.Claims;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        //
         long l = -9223372036854775808L;
         System.out.println(l);
+        otherP();
 
 // 主分支修改一些信息
+    }
+    public static void otherP(){
+        System.out.println("小红开发的代码");
     }
 
 
